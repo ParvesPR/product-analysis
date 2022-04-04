230b943f20e9e90892351b40f778b69ce0da64f0
ParvesPR <parves2585@gmail.com>
2022-04-04 13:39:02 +0800

    Home components added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Home.css';
+import Camera from '../../Images/camera.jpg'
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className="banner">
+                <div className="banner-text">
+                    <h1 className='first-banner-title'>Your Next Choice</h1>
+                    <h1 className='second-banner-title'>Your Next Camera</h1>
+                    <p>Capture the peaks of more decisive moments with the α7 III from Sony packing newly developed back-illuminated full-frame CMOS sensor  and other advanced imaging innovations, high-speed response, ease of operation, and reliable durability that are ready for various shooting needs.</p>
+                    <button className='banner-btn'>Live Demo</button>
+                </div>
+                <div className="banner-img">
+                    <img src={Camera} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
